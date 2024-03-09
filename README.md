be5e5f46093b87d511f82756f28c862177f9fbb1
Damian Bielecki <bielik20@gmail.com>
2024-03-09 14:27:59 +0100

    tmp

--- README.md
+++ README.md
@@ -17,3 +17,21 @@ Sidebery configuration for Firefox based on:
 1. Open Sidebery settings
 2. Go to `Help` -> Click `Import addon data` -> `/sidebery/settings.json`
 3. Go to `Styles editor` -> paste content of `/sidebery/styles.css` to editor tab
+
+### Floorp
+
+https://www.reddit.com/r/Floorp/comments/1b1tqsd/possible_to_remove_the_thin_black_bar_at_the_top/
+
+### Shortcuts
+
+https://support.mozilla.org/en-US/kb/keyboard-shortcuts-perform-firefox-tasks-quickly
+
+* [New Private Window ] cmd + shift + p -> (clear)
+* [New Window] cmd + n -> [New Private Window]
+* [Go one Tab to the Left/Right] cmd + opt + arrows -> (clear)
+* [Inspector] cmd + shift + c -> (clear) (I want to use it for copy url just like Arc does - I don't know if I need extension, or I can do it myself)
+* [Go to Bottom of Page/Go to Top of Page] cmd + arrow up/down -> (clear)
+* [Go Up/Down a Screen ] -> (clear)
+* [Library] cmd + y -> [Redo]
+* [Go one Tab to the Left/Right] -> (clear)
+* [Save Page As] cmd + s -> (clear) / set to toggle sidebery (sidebar) just like Arc
---
+++ floorp/profile/user.js
@@ -0,0 +1,13 @@
+
+
+/****************************************************************************
+ * SECTION: FLOORP                                                          *
+ ****************************************************************************/
+user_pref("browser.newtabpage.activity-stream.floorp.background.type", 0);
+user_pref("floorp.Tree-type.verticaltab.optimization", true);
+user_pref("floorp.browser.sidebar2.data", {"data":{"w202412473116":{"url":"extension,Bitwarden,{446900e4-71c2-419f-a6a7-df9c091e268b},moz-extension://cca45eb9-37a6-4eb7-a73b-1c8c60da5d0e/popup/index.html?uilocation=sidebar,moz-extension://cca45eb9-37a6-4eb7-a73b-1c8c60da5d0e/images/icon19.png"},"w202412473143":{"url":"extension,Raindrop.io,jid0-adyhmvsP91nUO8pRv0Mn2VKeB84@jetpack,moz-extension://13f166ee-b0b9-4060-a814-fb51f218ac5e/sidepanel.html,moz-extension://13f166ee-b0b9-4060-a814-fb51f218ac5e/assets/action_firefox_light_32.png","width":415},"floorp__bookmarks":{"url":"floorp//bookmarks","width":413},"floorp__history":{"url":"floorp//history","width":415},"floorp__downloads":{"url":"floorp//downloads","width":415},"w0":{"url":"https://translate.google.com"}},"index":["w202412473116","w202412473143","floorp__bookmarks","floorp__history","floorp__downloads","w0"]});
+user_pref("floorp.delete.browser.border", true);
+user_pref("floorp.extensions.allowPrivateBrowsingByDefault.is.enabled", true);
+user_pref("floorp.hide.unifiedExtensionsButtton", false);
+user_pref("floorp.tabsleep.enabled", true);
+
